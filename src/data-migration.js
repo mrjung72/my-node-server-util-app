ae58604f60a91faa53b206d84218cee0e8dd0794
sahara72 <mrjung72@gmail.com>
2025-06-09 16:57:13 +0900

    로깅 기능 추가

--- src/data-migration.js
+++ src/data-migration.js
@@ -36,50 +36,63 @@ if (!fs.existsSync(tableFilePath)) {
 
 async function migrateData() {
   try {
-
+    
     const tableNames = fs.readFileSync(tableFilePath, 'utf-8')
-      .split('\n')
-      .map(line => line.trim())
-      .filter(Boolean);
-
+    .split('\n')
+    .map(line => line.trim())
+    .filter(line => /^[a-zA-Z]/.test(line)); 
+    
     if (tableNames.length === 0) {
       console.error('❌ tables.txt 파일에 테이블이 없습니다.');
       return;
     }
-
+    
     const mssqlPool = await mssql.connect(mssqlConfig);
     const mariadbConn = await mysql.createConnection(mariadbConfig);
+    const [curr_time] = await mariadbConn.query(`select DATE_FORMAT(now(), '%y%m%d%H%i%s') curr_time `)
+    const work_id = `DM_${curr_time[0].curr_time}`
+    console.log('work_id:', work_id)
 
     for (const tableName of tableNames) {
-      console.log(`\n🚚 [${tableName}] 데이터 복사 중...`);
 
-      const mssqlResult = await mssqlPool.request().query(`SELECT TOP 100 * FROM [${tableName}]`);
+      console.log(`\n🚚 [${tableName}] 테이블 Truncate`);
+      await mariadbConn.query(`TRUNCATE TABLE \`${tableName}\``);
+
+      console.log(`🚚 [${tableName}] 데이터 가져오는 중...`);
+      const mssqlResult = await mssqlPool.request().query(`SELECT TOP 10 * FROM [${tableName}] WITH (NOLOCK)`);
       const rows = mssqlResult.recordset;
 
       if (rows.length === 0) {
         console.log(`⚠️ [${tableName}] 테이블에 데이터 없음. 건너뜀.`);
         continue;
       }
-
+      
       const columns = Object.keys(rows[0]);
       const placeholders = columns.map(() => '?').join(',');
       const insertSQL = `INSERT INTO \`${tableName}\` (${columns.map(c => `\`${c}\``).join(',')}) VALUES (${placeholders})`;
-
+     
       const insertValues = rows.map(row => columns.map(col => row[col]));
-
+      const logSQL = `INSERT INTO data_migration_log (work_id, table_name, data_rows, result_desc) VALUES (?, ?, ?, ?)`;
+      let resultMsg = ''
+      
       // 트랜잭션 시작
-        try {
-            await mariadbConn.beginTransaction();
-            for (const values of insertValues) {
-                await mariadbConn.query(insertSQL, values);
-            }
-            await mariadbConn.commit();
-        } catch (err) {
-            await mariadbConn.rollback();
-            console.error(`❌ [${tableName}] 트랜잭션 롤백됨:`, err.message);
-        }
+      try {
+        await mariadbConn.beginTransaction();
 
-        console.log(`✅ [${tableName}] ${rows.length}행 복사 완료.`);
+        for (const values of insertValues) {
+          await mariadbConn.query(insertSQL, values);
+        }
+        await mariadbConn.commit();
+        resultMsg = `✅ [${tableName}] ${rows.length}행 데이터이관 완료.`
+        console.log(resultMsg);
+
+      } catch (err) {
+          await mariadbConn.rollback();
+          resultMsg = `❌ [${tableName}] 트랜잭션 롤백됨: ${err.message}`
+          console.error(resultMsg);
+      } finally {
+          await mariadbConn.query(logSQL, [work_id, tableName, rows.length, resultMsg]);
+      }
     }
 
     await mssqlPool.close();
